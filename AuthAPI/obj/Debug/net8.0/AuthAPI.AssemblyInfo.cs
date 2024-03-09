1cef8de0270a9aa35d93c7c786016f98382e81af
oplaalvarez0001 <oplaalvarez@microsoft.com>
2024-03-08 21:42:38 -0500

    AuthN

--- AuthAPI/obj/Debug/net8.0/AuthAPI.AssemblyInfo.cs
+++ AuthAPI/obj/Debug/net8.0/AuthAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc22013c08400445f3077749d022846c66deafa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
