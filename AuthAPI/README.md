2c69a89a607d87f8d4f6194188bbbf8654269bfb
oplaalvarez0001 <oplaalvarez@microsoft.com>
2024-03-11 11:27:32 -0400

    Remove keys from git checkin

--- AuthAPI/README.md
+++
@@ -1,8 +0,0 @@
-Dockerize: https://code.visualstudio.com/docs/containers/quickstart-aspnet-core
-
-
-Docker is online on  builded code & configuration so swagger will not be avail on pod
-
-
-To enble certs on containers 
-https://learn.microsoft.com/en-us/aspnet/core/security/docker-https?view=aspnetcore-8.0
--- AuthAPI/obj/Debug/net8.0/AuthAPI.AssemblyInfo.cs
+++ AuthAPI/obj/Debug/net8.0/AuthAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cef8de0270a9aa35d93c7c786016f98382e81af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd859bac605f112fba5f3f68e0f5ecc7eac6676")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
